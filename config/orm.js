20639e17d01795ec721f2f3f4db46b217e811da0
Ray Luna <leon.luna.ray@gmail.com>
2021-03-18 23:50:25 -0700

    initial setup complete

---
+++ config/orm.js
@@ -0,0 +1,55 @@
+// Connection
+const connection = require('./connection');
+  
+  // Object for all our SQL statement functions.
+  const orm = {
+    selectAll: (tableInput, cb) => {
+      const queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne: (table, cols, vals, cb) => {
+      const queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+
+    updateOne: function(table, objColVals, condition, cb) {
+      const queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
+  
+  module.exports = orm;  
