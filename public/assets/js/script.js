2038a8813cd4c6bedc68f2553129f7eb49c2eec0
Ray Luna <leon.luna.ray@gmail.com>
2021-04-03 15:35:32 -0700

    setup boilerplate frontend

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,17 @@
+// Buttons
 const orderBtn = document.getElementById('order-btn');
-orderBtn.addEventListener('click', orderBurger);
+const eatBtn = document.getElementById('eat-btn');
 
+// Event listeners
+orderBtn.addEventListener('click', orderBurger);
+eatBtn.addEventListener('click', eatDaBurger);
+
+// Order the burger and add to burgers list
 function orderBurger() {
     console.log('🍔 Burger ordered!')  
 }; 
+
+// Eat burger and move to devoured
+function eatDaBurger() {
+    console.log('✅ Burger Devoured!')
+};
