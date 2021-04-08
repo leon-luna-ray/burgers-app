f11aff9835ec297b7744c44f8a5079fd0637461c
Ray Luna <leon.luna.ray@gmail.com>
2021-04-07 17:34:48 -0700

    update heroku db conneciton

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,18 @@ const mysql = require('mysql');
 // Bug with password not populating from .env
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'burger_db'
+
+  HOST: "us-cdbr-east-03.cleardb.com",
+  USER: "bb732f0d5ba7b2",
+  PASSWORD: "ab163585",
+  DB: "heroku_344581484ebadd5"
+
+  // Local envoirnment connection
+  // host: 'localhost',
+  // port: 3306,
+  // user: 'root',
+  // password: process.env.DB_PASSWORD,
+  // database: 'burger_db'
 });
 
 // Make connection.
