b90bfe878ad4bbc7b1bf667fe3312e78b867f007
Ray Luna <leon.luna.ray@gmail.com>
2021-04-07 21:45:43 -0700

    eslint

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,28 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
+let connection;
+
+// Connection will use jawsdb for production or local for development
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'burger_db'
+    database: 'burger_db',
   });
-};
-// Make connection.
+}
+// Make connection
 connection.connect((err) => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    // eslint-disable-next-line no-console
+    console.error(`error connecting: ' + ${err.stack}`);
     return;
   }
+  // eslint-disable-next-line no-console
   console.log(`
     ✅ Connected to DB as id ${connection.threadId}`);
 });
