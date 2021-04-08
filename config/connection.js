b06ade904a59183b432e9bac1ab2df95a2437fcf
Ray Luna <leon.luna.ray@gmail.com>
2021-04-07 21:23:44 -0700

    update conneciton

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,17 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-// Bug with password not populating from .env
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'burger_db'
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'burger_db'
+  });
+};
 // Make connection.
 connection.connect((err) => {
   if (err) {
